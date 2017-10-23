d4782aa0ee0ada5bdf847a317d4365de1d038209
Hamza Aid <hamza@appsdoc.com>
2017-10-23 17:58:14 +0100

    Add cordova recording plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { MediaPlugin } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
+  public startRecording() {
+	  try {
+	    let media = new MediaPlugin('../Library/NoCloud/recording.wav');
+	    media.startRecord();
+	  }
+	  catch (e) {
+	    this.showAlert('Could not start recording.');
+	  }
+	}
+
+	public showAlert(message) {
+	  let alert = this.alertCtrl.create({
+	    title: 'Error',
+	    subTitle: message,
+	    buttons: ['OK']
+	  });
+	  alert.present();
+	}
 }
